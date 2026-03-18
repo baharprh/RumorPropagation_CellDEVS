942c253de9aee57bb34948f052384543c5eff82b
baharprh <bpour072@uottawa.ca>
2026-03-18 02:57:40 -0400

    Update main.cpp

--- RumorPropagation_Assign2/src/main.cpp
+++ RumorPropagation_Assign2/src/main.cpp
@@ -1,10 +1,7 @@
 #include <fstream>
-#include <vector>
 #include <cadmium/modeling/dynamic_coupled.hpp>
 #include <cadmium/engine/pdevs_dynamic_runner.hpp>
 #include <cadmium/logger/common_loggers.hpp>
-#include <web/web_logger.hpp>
-#include <web/web_results.hpp>
 #include "rumor_coupled.hpp"
 
 using namespace std;
@@ -13,51 +10,31 @@ using namespace cadmium::celldevs;
 
 using TIME = double;
 
-/*************** Log files *******************/
-static ofstream out_messages("../viewer/output_messages.txt");
+/*************** Loggers *******************/
+static ofstream out_messages("../logs/output_messages.txt");
 struct oss_sink_messages {
     static ostream& sink() {
         return out_messages;
     }
 };
 
-static ofstream out_state("../viewer/state_messages.txt");
+static ofstream out_state("../logs/state.txt");
 struct oss_sink_state {
     static ostream& sink() {
         return out_state;
     }
 };
 
-/*************** Web-format loggers *******************/
-using web_state = cadmium::web::logger<
-    cadmium::logger::logger_state,
-    cadmium::web::formatter<TIME>,
-    oss_sink_state
->;
+using state_logger = logger::logger<logger::logger_state, logger::formatter<TIME>, oss_sink_state>;
+using log_messages = logger::logger<logger::logger_messages, logger::formatter<TIME>, oss_sink_messages>;
+using global_time_messages = logger::logger<logger::logger_global_time, logger::formatter<TIME>, oss_sink_messages>;
+using global_time_state = logger::logger<logger::logger_global_time, logger::formatter<TIME>, oss_sink_state>;
 
-using web_log_messages = cadmium::web::logger<
-    cadmium::logger::logger_messages,
-    cadmium::web::formatter<TIME>,
-    oss_sink_messages
->;
-
-using web_global_time_mes = cadmium::web::logger<
-    cadmium::logger::logger_global_time,
-    cadmium::web::formatter<TIME>,
-    oss_sink_messages
->;
-
-using web_global_time_sta = cadmium::web::logger<
-    cadmium::logger::logger_global_time,
-    cadmium::web::formatter<TIME>,
-    oss_sink_state
->;
-
-using logger_top = cadmium::logger::multilogger<
-    web_state,
-    web_log_messages,
-    web_global_time_mes,
-    web_global_time_sta
+using logger_top = logger::multilogger<
+    state_logger,
+    log_messages,
+    global_time_messages,
+    global_time_state
 >;
 
 int main(int argc, char **argv) {
@@ -76,17 +53,9 @@ int main(int argc, char **argv) {
         std::make_shared<rumor_coupled<TIME>>(test);
 
     cadmium::dynamic::engine::runner<TIME, logger_top> r(t, {0});
-    float sim_time = (argc > 2) ? atof(argv[2]) : 50;
+    float sim_time = (argc > 2) ? static_cast<float>(atof(argv[2])) : 50.0f;
     r.turn_progress_on();
     r.run_until(sim_time);
-
-    std::vector<std::string> fields = {"state"};
-    cadmium::web::convert_cell_devs(
-        fields,
-        scenario_config_file_path,
-        "../viewer/state_messages.txt",
-        "../viewer/"
-    );
 
     return 0;
 }
