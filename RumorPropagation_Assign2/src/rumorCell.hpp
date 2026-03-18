6adac30b6d6293d1f4c748b21cdc6a439abfae21
baharprh <bpour072@uottawa.ca>
2026-03-18 03:08:41 -0400

    Update rumorCell.hpp

--- RumorPropagation_Assign2/src/rumorCell.hpp
+++ RumorPropagation_Assign2/src/rumorCell.hpp
@@ -1,61 +1,17 @@
-#ifndef CADMIUM_CELLDEVS_RUMOR_CELL_HPP
-#define CADMIUM_CELLDEVS_RUMOR_CELL_HPP
-
-#include <cadmium/celldevs/cell/grid_cell.hpp>
-
-using namespace cadmium::celldevs;
-
-template <typename T>
-class rumor_cell : public grid_cell<T, int, int> {
-public:
-    using grid_cell<T, int, int>::simulation_clock;
-    using grid_cell<T, int, int>::state;
-    using grid_cell<T, int, int>::map;
-    using grid_cell<T, int, int>::neighbors;
-
-    int spreading_time;
-
-    rumor_cell() : grid_cell<T, int, int>(), spreading_time(1) {}
-
-    rumor_cell(
-        cell_position const &cell_id,
-        cell_unordered<int> const &neighborhood,
-        int initial_state,
-        cell_map<int, int> const &map_in,
-        std::string const &delay_id,
-        int config
-    ) : grid_cell<T, int, int>(cell_id, neighborhood, initial_state, map_in, delay_id),
-        spreading_time(config) {}
-
-    int local_computation() const override {
-        int current = state.current_state;
-
-        // 2 = inactive stays inactive
-        if (current == 2) {
-            return 2;
-        }
-
-        // 1 = spreading becomes inactive
-        if (current == 1) {
-            return 2;
-        }
-
-        // 0 = unaware becomes spreading if any neighbor is spreading
-        if (current == 0) {
-            for (auto neighbor : neighbors) {
-                if (state.neighbors_state.at(neighbor) == 1) {
-                    return 1;
-                }
-            }
-            return 0;
-        }
-
-        return current;
-    }
-
-    T output_delay(int const &cell_state) const override {
-        return T(1);
-    }
+struct rumorState {
+    int state;
+    rumorState() : state(0) {}
 };
 
-#endif
+inline std::ostream& operator<<(std::ostream& os, const rumorState& x) {
+    os << "<" << x.state << ">";
+    return os;
+}
+
+inline bool operator!=(const rumorState& x, const rumorState& y) {
+    return x.state != y.state;
+}
+
+inline void from_json(const nlohmann::json& j, rumorState& s) {
+    j.at("state").get_to(s.state);
+}
